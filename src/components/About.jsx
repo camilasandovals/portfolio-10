d648ff820acbe21a8f7149d959baf5790a46461e
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-26 13:58:43 -0400

    added header

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,20 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 export default function About() {
     return (
         <section>
-            <h1>About</h1>
+            <Container className="bg-danger">
+                <Row>
+                    <Col>
+                        <img src= "/images/profile22.png" alt="profile pict" width={300}></img>
+                    </Col>
+
+                    <Col>
+                    <h1>Hi, I am Camila</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum saepe ratione qui amet a soluta, est excepturi modi cumque animi rerum itaque blanditiis, illum aliquid recusandae tempora. Iusto, quos explicabo!</p>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     )
 }
