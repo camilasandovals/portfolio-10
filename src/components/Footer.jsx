594c7a08d3f2cb0bf77e37766e9969e15d637fb9
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-26 16:58:08 -0400

    added style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
+import { Github } from "react-bootstrap-icons"
+
 export default function About() {
+    const githubUrl = "https://github.com/milasandovals/"
     const currentYear = new Date().getFullYear()
     return (
         <footer>
+            <p className="text-center">
             <small>&copy; {currentYear} - Camila Sandoval</small>
+            <a href={githubUrl}
+                target="_blank"
+                rel="noreferrer"
+                className="button-effect">
+            <Github size = "60" color="black"/>
+            </a>
+            </p>
         </footer>
     )
 }
